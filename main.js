aa13a31e4a5d18d321709d73051c655ae6ac9bac
Ecline Jeruto koech <eclinejerutokoech@gmail.com>
2021-05-04 00:44:56 +0300

    background size for monthly earnings set to contain

--- main.js
+++ main.js
@@ -7,6 +7,23 @@ function totalDailyProduction(){
     if(!shadeA || !shadeB || !shadeC || !shadeD){
         alert("Please fill all the boxes");
         return;
-    }    
+    }
     
+    totalDailyProduction.push({name:"Shade A",litres:shadeA},{name:"Shade B",litres:shadeB},{name:"Shade C",litres:shadeC}{name:"Shade D",litres:shadeD});
+    return sum;
 }
+
+var leapYear = [
+    {month:"January", days:31},
+    {month:"February", days:29},
+    {month:"March", days:31},
+    {month:"April", days:30},
+    {month:"May", days:31},
+    {month:"June", days:30},
+    {month:"July", days:31},
+    {month:"August", days:31},
+    {month:"September", days:30},
+    {month:"October", days:31},
+    {month:"November", days:30},
+    {month:"December", days:31},
+]
