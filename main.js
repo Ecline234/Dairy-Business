406098cb845f5455a2e848b96caee5ac20d39c98
Ecline Jeruto koech <eclinejerutokoech@gmail.com>
2021-05-04 07:30:27 +0300

    changed height of body to 400vh

--- main.js
+++ main.js
@@ -7,12 +7,10 @@ function totalDailyProduction(){
     if(!shadeA || !shadeB || !shadeC || !shadeD){
         alert("Please fill all the boxes");
         return;
-    }
-    
+    } 
     totalDailyProduction.push({name:"Shade A",litres:shadeA},{name:"Shade B",litres:shadeB},{name:"Shade C",litres:shadeC}{name:"Shade D",litres:shadeD});
     return sum;
 }
-
 var leapYear = [
     {month:"January", days:31},
     {month:"February", days:29},
@@ -25,5 +23,6 @@ var leapYear = [
     {month:"September", days:30},
     {month:"October", days:31},
     {month:"November", days:30},
-    {month:"December", days:31},
-]
+    {month:"December", days:31}];
+
+    let price = 45;
